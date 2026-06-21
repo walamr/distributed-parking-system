c470304f67f11933902ba650282531923994aa05
walamr <wmruwat@gmail.com>
2026-06-21 16:47:03 +0300

    feat: add batch scripts to automate startup and health checks for MongoDB containers

--- quick-start/01-run-mongo1.bat
+++ quick-start/01-run-mongo1.bat
@@ -3,6 +3,14 @@ cd %~dp0..
 echo Starting MongoDB Node 1 container...
 docker compose --env-file network-ips.env -f docker-compose.mongo1.yml up -d
 echo MongoDB Node 1 is running.
-echo Waiting 40 seconds for MongoDB to start...
-timeout /t 40 >nul
+echo Waiting for MongoDB Node 1 to be ready...
+
+:WAIT_LOOP
+docker exec mongo1 mongosh --tls --tlsAllowInvalidCertificates --tlsAllowInvalidHostnames --tlsCAFile /etc/mongo/certs/ca-cert.pem --tlsCertificateKeyFile /etc/mongo/certs/mongo1.pem --host localhost --port 27017 --eval "db.runCommand({ping: 1})" --quiet >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    timeout /t 1 >nul
+    goto WAIT_LOOP
+)
+
+echo MongoDB Node 1 is ready!
 call .\interactive_queries.bat 1
--- quick-start/03-run-mongo3.bat
+++ quick-start/03-run-mongo3.bat
@@ -3,6 +3,14 @@ cd %~dp0..
 echo Starting MongoDB Node 3 container...
 docker compose --env-file network-ips.env -f docker-compose.mongo3.yml up -d
 echo MongoDB Node 3 is running.
-echo Waiting 40 seconds for MongoDB to start...
-timeout /t 40 >nul
+echo Waiting for MongoDB Node 3 to be ready...
+
+:WAIT_LOOP
+docker exec mongo3 mongosh --tls --tlsAllowInvalidCertificates --tlsAllowInvalidHostnames --tlsCAFile /etc/mongo/certs/ca-cert.pem --tlsCertificateKeyFile /etc/mongo/certs/mongo3.pem --host localhost --port 27017 --eval "db.runCommand({ping: 1})" --quiet >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    timeout /t 1 >nul
+    goto WAIT_LOOP
+)
+
+echo MongoDB Node 3 is ready!
 call .\interactive_queries.bat 3
